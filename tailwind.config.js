2e678cf81df348f72c03d5e61942c7aac50bbcd0
Paulina Siwko <pau.siwko@gmail.com>
2024-04-15 16:02:28 +0200

    setting up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./index.html",
-  "./highscores.html",
-  "./src/**/*.{html,js}"
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
-
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customPink: '#efebf2',
+        customGrey: '#d1cfd3',
+        customPurple: '#30105a',
+      }
+    },
+  },
+  plugins: [],
+}
